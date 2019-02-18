b7463bfffb5836057984ea6e6e46c3233fdff8ca
deathcode <carotkut12@gmail.com>
2019-02-18 12:52:23 +0530

    Added handler for change and submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  state = {};
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submit");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={this.state.account.username}
+              autoFocus
+              onChange={this.handleChange}
+              ref={this.username}
+              id="username"
+              type="text"
+              name="username"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={this.state.account.password}
+              ref={this.password}
+              id="password"
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
